d4bbe4e57128774732bbc99641f978e1629d5e53
wwj <614505891@qq.com>
2021-12-25 11:30:19 +0800

    '【架构师】【新开一个项目给apt课程】'

---
+++ 02 - ZhuJie/ZhuJieAPT/app/src/main/java/com/example/zhujieapt/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.zhujieapt;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_main_content)
+    TextView tvMainContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+}

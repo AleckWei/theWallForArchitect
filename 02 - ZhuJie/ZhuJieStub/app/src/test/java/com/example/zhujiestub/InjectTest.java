e124f0be44ddde13d88e877a1f6f356c23e4c78c
wwj <614505891@qq.com>
2021-12-30 23:27:39 +0800

    '【架构师】【插装技术开头】'

---
+++ 02 - ZhuJie/ZhuJieStub/app/src/test/java/com/example/zhujiestub/InjectTest.java
@@ -0,0 +1,10 @@
+package com.example.zhujiestub;
+
+public class InjectTest {
+
+    @ASMTest
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1_0000);
+    }
+
+}

a3630442c4a7c79204a7baf9bb575a50214021ac
wwj <614505891@qq.com>
2022-01-04 23:42:05 +0800

    '【架构师】【字节码插桩已经完成，下一步就是结合注解针对特定注解进行插桩】'

--- 02 - ZhuJie/ZhuJieStub/app/src/test/java/com/example/zhujiestub/ASMUnitTest.java
+++ 02 - ZhuJie/ZhuJieStub/app/src/test/java/com/example/zhujiestub/ASMUnitTest.java
@@ -6,7 +6,9 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.AdviceAdapter;
+import org.objectweb.asm.commons.Method;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -77,6 +79,9 @@ public class ASMUnitTest {
      * 这里才能对method的方法体进行插桩
      */
     static class MyMethodVisitor extends AdviceAdapter {
+
+        private int startTimeIndex;
+
         protected MyMethodVisitor(int api, MethodVisitor methodVisitor, int access, String name, String descriptor) {
             super(api, methodVisitor, access, name, descriptor);
         }
@@ -89,6 +94,18 @@ public class ASMUnitTest {
         protected void onMethodEnter() {
             super.onMethodEnter();
             // 插入 long startTime = System.currentTimeMillis();
+            // 这里要涉及方法签名的知识,如果是普通的类，则需要要在路径前加一个L
+            // new Method,指的是调用对应类中的方法，参数一是方法名，参数二是方法签名
+            // 这个方法签名由 参数类型 + 方法返回值签名 组成
+            invokeStatic(Type.getType("Ljava/lang/System;"),
+                    new Method("currentTimeMillis", "()J"));
+            // 这样就插入了一个 System.currentTimeMillis 的字节码
+
+            // 先创建一个本地变量的索引(Long startTime)
+            startTimeIndex = newLocal(Type.LONG_TYPE);
+
+            // 让上一步执行的结果，用一个本地变量接收(startTime = System.currentTimeMillis)
+            storeLocal(startTimeIndex);
         }
 
         /**
@@ -99,6 +116,35 @@ public class ASMUnitTest {
             super.onMethodExit(opcode);
             // 插入 long endTime = System.currentTimeMillis();
             // System.out.println("execute: " + (endTime - startTime) + "ms.");
+
+            // 同理，在方法退出时，再插入一个获取时间
+            invokeStatic(Type.getType("Ljava/lang/System;"),
+                    new Method("currentTimeMillis", "()J"));
+            int end = newLocal(Type.LONG_TYPE);
+            storeLocal(end);
+
+            getStatic(Type.getType("Ljava/lang/System;"), "out", Type.getType("Ljava/io/PrintStream;"));
+            newInstance(Type.getType("Ljava/lang/StringBuilder;"));
+            dup();
+            invokeConstructor(Type.getType("Ljava/lang/StringBuilder;"),
+                    new Method("<init>", "()V"));
+            visitLdcInsn("execute: ");
+            invokeVirtual(Type.getType("Ljava/lang/StringBuilder;"),
+                    new Method("append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;"));
+            loadLocal(end);
+            loadLocal(startTimeIndex);
+            math(SUB, Type.LONG_TYPE);
+
+            invokeVirtual(Type.getType("Ljava/lang/StringBuilder;"),
+                    new Method("append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;"));
+
+            visitLdcInsn(" ms.");
+            invokeVirtual(Type.getType("LMethod java/lang/StringBuilder;"),
+                    new Method("append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;"));
+            invokeVirtual(Type.getType("LMethod java/lang/StringBuilder;"),
+                    new Method("toString", "()Ljava/lang/String;"));
+            invokeVirtual(Type.getType("Ljava/io/PrintStream;"),
+                    new Method("println", "(Ljava/lang/String;)V"));
         }
     }
 }
--- 02 - ZhuJie/ZhuJieStub/app/src/test/java/com/example/zhujiestub/InjectTest.java
+++ 02 - ZhuJie/ZhuJieStub/app/src/test/java/com/example/zhujiestub/InjectTest.java
@@ -2,7 +2,6 @@ package com.example.zhujiestub;
 
 public class InjectTest {
 
-    @ASMTest
     public static void main(String[] args) throws InterruptedException {
         Thread.sleep(1_0000);
     }

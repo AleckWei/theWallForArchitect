ed59422745a92d08f5bac51ab3627ef29c05aacf
wwj <614505891@qq.com>
2022-01-22 22:09:18 +0800

    '【架构师】【听完剩下的ThreadLocal,每个线程只会获取自己的那份拷贝的数据（数据量较小的情况）】'

---
+++ 03 - Thread/app/src/test/java/com/example/thread/UserThreadLocal.java
@@ -0,0 +1,56 @@
+package com.example.thread;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.junit.Test;
+
+public class UserThreadLocal {
+
+    // 保证每个线程获取到本类的对象时，都会有一个初始的变量且默认值为 1
+    static ThreadLocal<Integer> threadLocal = new ThreadLocal<Integer>() {
+        @NonNull
+        @Override
+        protected Integer initialValue() {
+            return 1;
+        }
+    };
+
+    private void startThreadArray() {
+        Thread[] threads = new Thread[3];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new TestThread(i));
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+    }
+
+    private static class TestThread implements Runnable {
+
+        int id;
+
+        public TestThread(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + " start running");
+            Integer integer = threadLocal.get();
+            integer += id;
+            threadLocal.set(integer);
+            System.out.println(Thread.currentThread().getName() + " value： " + threadLocal.get());
+            threadLocal.remove();
+        }
+    }
+
+    @Test
+    public void main() {
+        UserThreadLocal userThreadLocal = new UserThreadLocal();
+        userThreadLocal.startThreadArray();
+    }
+
+
+}
